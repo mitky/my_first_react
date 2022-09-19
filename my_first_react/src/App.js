09509ba2e54b51a9815073ec490221f1fdd0e4b2
mitky <dimitar.shikrenov@gmail.com>
2022-09-19 11:39:02 +0100

    cards Sets

--- my_first_react/src/App.js
+++ my_first_react/src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
+
+  const [value, setValue] = React.useState('Promotional');
+
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div>
+      <label>
+      Card Set: 
+        <select   value={value} onChange={handleChange}>
+          <option value="Promotional">Promotional</option>
+          <option value="The Fellowship of the Ring">The Fellowship of the Ring</option>
+          <option value="Mines of Moria">Mines of Moria</option>
+          <option value="Realms of the Elf-lords">Realms of the Elf-lords</option>
+          <option value="The two Towers">The two Towers</option>
+          <option value="Battle of helo's Deep">Battle of helo's Deep</option>
+          <option value="Ents of Fangorn">Ents of Fangorn</option>
+          <option value="The Return of the King">The Return of the King</option>
+          <option value="Siege of Gondor">Siege of Gondor</option>
+          <option value="Reflections">Reflections</option>
+          <option value="Mount Doom">Mount Doom</option>
+          <option value="Shadows">Shadows</option>
+          <option value="Black Rider">Black Rider</option>
+          <option value="Bloodlones">Bloodlones</option>
+          <option value="Expanded Middle-earth">Expanded Middle-earth</option>
+          <option value="The Hunters">The Hunters</option>
+          <option value="The Wraith Collection">The Wraith Collection</option>
+          <option value="Rise of Saruman">Rise of Saruman</option>
+          <option value="Treachery & Deceit">Treachery & Deceit</option>
+          <option value="Ages End">Ages End</option>
+      </select>
+    </label>
+    <p>Selected set: {value}</p>
+  </div>
+    </>
   );
 }
 
